73a2b77b5546f735ad849e95b8be8722a3d87f40
Carlos Eduardo <ducameneses16@gmail.com>
2023-09-30 16:17:48 -0300

    style: :lipstick: Fazendo alteração nas cores do projeto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
     
         {/*Container*/}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-[#4d7cf4]'>Oi,👋 meu nome é </p>
+            <p className='text-[#0a192f]'>Oi,👋 meu nome é </p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Carlos Eduardo</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#0a192f]'>Eu sou um desenvolvedor Full Stack.</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#E5D283]'>Eu sou um desenvolvedor Full Stack.</h2>
             <p className='text-[#0a192f] py-4 max-w-[700px]'>Sou um desenvolvedor full-stack em busca de oportunidades para contribuir em projetos e aprimorar minhas habilidades junto a desenvolvedores experientes no mercado de desenvolvimento.</p>
         <div>
             <button className='text-[#0a192f] group font-bold border-2 border-[#0a192f] px-6 py-3 flex items-center hover:bg-[#0a192f] hover:text-white'>
